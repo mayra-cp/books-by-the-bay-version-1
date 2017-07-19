8b96b37ce7fe33d6dab02e3d38184f2df8b9c549
Mayra <mayraacm24@gmail.com>
2017-07-18 21:49:06 -0700

    initial changes
    first changes on project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Make it rain!
+
+var selectOne = document.getElementById("select-one");
+var selectTwo = document.getElementById("select-two");
+var selectThree = document.getElementById("select-three");
+
+$('.question-prompt').on('click' function () {
+	$('.arrow').slidedown
+})
