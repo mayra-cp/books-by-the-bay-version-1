560c8bf02fcace4d377c632f052a15f65a3a3a36
Mayra <mayraacm24@gmail.com>
2017-07-19 20:05:00 -0700

    fixed arrow issue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
 // Make it rain!
 
-var selectOne = document.getElementById("select-one");
-var selectTwo = document.getElementById("select-two");
-var selectThree = document.getElementById("select-three");
+// var selectOne = document.getElementById("select-one");
+// var selectTwo = document.getElementById("select-two");
+// var selectThree = document.getElementById("select-three");
 
-$('.question-prompt').on('click' function () {
-	$('.arrow').slidedown
+// $('img').click(function () {
+// 		$(this).toggleClass('sampleClass');
+// 		console.log('clicked!');
+// 	})
+
+
+$(document).ready(function () {
+	console.log("ready!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 })
+
