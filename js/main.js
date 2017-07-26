736eb33a2ece0e10f84bae5fb1344732e6926f8d
Mayra <mayraacm24@gmail.com>
2017-07-25 23:08:41 -0700

    Final edits

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 var answers = [];
 
 $(document).ready(function() {
-	
 
 	$('.select-button').on('click', function() {
 		$('.dropdown-content').toggle()
 	});
 
+	$('.arrow').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('#question-one').offset().top
+		}, 1000);
+	})
 
 	$('.options').on('click', function() {
 		$(this).parents('.question').hide();
@@ -15,46 +19,44 @@ $(document).ready(function() {
 
 		if ( $(this).parents('.question').is('#question-three') === true) {
 
-			if (answers[0] === "Old" && answers[1] === "Small" && answers[2] === "Yes") {
+			if (answers[0] === "New" && answers[1] === "Modern" && answers[2] === "Leisure") {
+				$('.bookstore-one').show()//show recommendation Chronicle Books//
+			}
+
+			else if (answers[0] === "Mix" && answers[1] === "Old" && answers[2] === "Learning") {
+				$('.bookstore-two').show()//show recommendation City Lights//
+			}
+
+			else if (answers[0] === "Mix" && answers[1] === "Old" && answers[2] === "Leisure") {
+				$('.bookstore-three').show()//show recommendation Russian Hill Bookstore//
+			}
+
+			else if (answers[0] === "New" && answers[1] === "Old" && answers[2] === "Leisure") {
+				$('.bookstore-four').show()//show recommendation Books Inc.//
+			}
+
+			else if (answers[0] === "Mix" && answers[1] === "Modern" && answers[2] === "Learning") {
+			$('.bookstore-five').show()//show recommendation Booksmith//
+			}
+
+			else if (answers[0] === "New" && answers[1] === "Old" && answers[2] === "Learning") {
+				$('.bookstore-six').show()//show recommendation The Green Arcade//
+			}
+
+			else if (answers[0] === "Mix" && answers[1] === "Modern" && answers[2] === "Leisure") {
 				$('.bookstore-seven').show()//show recommendation Dog Eared Books//
 			}
 
-			else if (answers[0] === "Old" && answers[1] === "Small" && answers[2] === "No") {
-			//show recommendation Dog Eared Books//
+			else if (answers[0] === "New" && answers[1] === "Modern" && answers[2] === "Learning") {
+				$('.bookstore-eight').show()//show recommendation Owl Cave Books//
 			}
 
-			else if (answers[0] === "Old" && answers[1] === "Large" && answers[2] === "No") {
-				console.log('City Lights')//show recommendation Dog Eared Books//
-			}
-
-			else if (answers[0] === "Old" && answers[1] === "Large" && answers[2] === "Yes") {
-				console.log('Russian Hill Bookstore')//show recommendation Dog Eared Books//
-			}
-
-			else if (answers[0] === "New" && answers[1] === "Small" && answers[2] === "Yes") {
-				console.log('Alley Cat Books')//show recommendation Dog Eared Books//
-			}
-
-			else if (answers[0] === "New" && answers[1] === "Small" && answers[2] === "No") {
-				console.log('Green Apple Bookstore')//show recommendation Dog Eared Books//
-			}
-
-			else if (answers[0] === "New" && answers[1] === "Large" && answers[2] === "No") {
-				console.log('Owl Cave Books')//show recommendation Dog Eared Books//
-			}
-
-			else if (answers[0] === "New" && answers[1] === "Large" && answers[2] === "Yes") {
-				console.log('Borderlands Books')//show recommendation Dog Eared Books//
-			}
-
+			$('.question-set').css('display', 'none');
 		}
 
 		else {
 			$(this).parents('.question').next().show();
 
-		}
-
-		
+		}	
 	});
-
 });
